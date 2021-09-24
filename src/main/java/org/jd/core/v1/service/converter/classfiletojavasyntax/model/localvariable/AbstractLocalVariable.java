7bd50cd4e57fe60f473b133acbe676de408173b5
luca-vercelli <luca.vercelli.to@gmail.com>
2021-09-24 23:11:57 +0200

    clean

--- src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/model/localvariable/AbstractLocalVariable.java
+++ src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/model/localvariable/AbstractLocalVariable.java
@@ -13,120 +13,159 @@ import org.jd.core.v1.util.DefaultList;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public abstract class AbstractLocalVariable {
-    protected Frame frame;
-    protected AbstractLocalVariable next;
-    protected boolean declared;
-    protected int index;
-    protected int fromOffset;
-    protected int toOffset;
-    protected String name;
-    protected DefaultList<LocalVariableReference> references = new DefaultList<>();
-    protected HashSet<AbstractLocalVariable> variablesOnRight = null;
-    protected HashSet<AbstractLocalVariable> variablesOnLeft = null;
+	protected Frame frame;
+	protected AbstractLocalVariable next;
+	protected boolean declared;
+	protected int index;
+	protected int fromOffset;
+	protected int toOffset;
+	protected String name;
+	protected DefaultList<LocalVariableReference> references = new DefaultList<>();
+	protected Set<AbstractLocalVariable> variablesOnRight = null;
+	protected Set<AbstractLocalVariable> variablesOnLeft = null;
 
-    public AbstractLocalVariable(int index, int offset, String name) {
-        this(index, offset, name, (offset == 0));
-    }
+	public AbstractLocalVariable(int index, int offset, String name) {
+		this(index, offset, name, (offset == 0));
+	}
 
-    public AbstractLocalVariable(int index, int offset, String name, boolean declared) {
-        this.declared = declared;
-        this.index = index;
-        this.fromOffset = offset;
-        this.toOffset = offset;
-        this.name = name;
-    }
+	public AbstractLocalVariable(int index, int offset, String name, boolean declared) {
+		this.declared = declared;
+		this.index = index;
+		this.fromOffset = offset;
+		this.toOffset = offset;
+		this.name = name;
+	}
 
-    public Frame getFrame() { return frame; }
-    public void setFrame(Frame frame) { this.frame = frame; }
+	public Frame getFrame() {
+		return frame;
+	}
 
-    public AbstractLocalVariable getNext() { return next; }
-    public void setNext(AbstractLocalVariable next) { this.next = next; }
+	public void setFrame(Frame frame) {
+		this.frame = frame;
+	}
 
-    public boolean isDeclared() { return declared; }
-    public void setDeclared(boolean declared) { this.declared = declared; }
+	public AbstractLocalVariable getNext() {
+		return next;
+	}
 
-    public int getIndex() { return index; }
+	public void setNext(AbstractLocalVariable next) {
+		this.next = next;
+	}
 
-    public int getFromOffset() { return fromOffset; }
+	public boolean isDeclared() {
+		return declared;
+	}
 
-    public void setFromOffset(int fromOffset) {
-        assert fromOffset <= toOffset;
-        this.fromOffset = fromOffset;
-    }
+	public void setDeclared(boolean declared) {
+		this.declared = declared;
+	}
 
-    public int getToOffset() { return toOffset; }
+	public int getIndex() {
+		return index;
+	}
 
-    public void setToOffset(int offset) {
-        if (this.fromOffset > offset)
-            this.fromOffset = offset;
-        if (this.toOffset < offset)
-            this.toOffset = offset;
-    }
+	public int getFromOffset() {
+		return fromOffset;
+	}
 
-    public void setToOffset(int offset, boolean force) {
-        this.toOffset = offset;
-    }
+	public void setFromOffset(int fromOffset) {
+		assert fromOffset <= toOffset;
+		this.fromOffset = fromOffset;
+	}
 
-    public abstract Type getType();
+	public int getToOffset() {
+		return toOffset;
+	}
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+	public void setToOffset(int offset) {
+		if (this.fromOffset > offset)
+			this.fromOffset = offset;
+		if (this.toOffset < offset)
+			this.toOffset = offset;
+	}
 
-    public abstract int getDimension();
+	public void setToOffset(int offset, boolean force) {
+		this.toOffset = offset;
+	}
 
-    public abstract void accept(LocalVariableVisitor visitor);
+	public abstract Type getType();
 
-    public DefaultList<LocalVariableReference> getReferences() { return references; }
-    public void addReference(LocalVariableReference reference) { references.add(reference); }
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Determines if the local variable represented by this object is either the same as, or is a super type variable
-     * of, the local variable represented by the specified parameter.
-     */
-    public abstract boolean isAssignableFrom(Map<String, BaseType> typeBounds, Type type);
-    public abstract void typeOnRight(Map<String, BaseType> typeBounds, Type type);
-    public abstract void typeOnLeft(Map<String, BaseType> typeBounds, Type type);
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public abstract boolean isAssignableFrom(Map<String, BaseType> typeBounds, AbstractLocalVariable variable);
-    public abstract void variableOnRight(Map<String, BaseType> typeBounds, AbstractLocalVariable variable);
-    public abstract void variableOnLeft(Map<String, BaseType> typeBounds, AbstractLocalVariable variable);
+	public abstract int getDimension();
 
-    protected void fireChangeEvent(Map<String, BaseType> typeBounds) {
-        if (variablesOnLeft != null) {
-            for (AbstractLocalVariable v : variablesOnLeft) {
-                v.variableOnRight(typeBounds, this);
-            }
-        }
-        if (variablesOnRight != null) {
-            for (AbstractLocalVariable v : variablesOnRight) {
-                v.variableOnLeft(typeBounds, this);
-            }
-        }
-    }
+	public abstract void accept(LocalVariableVisitor visitor);
 
-    protected void addVariableOnLeft(AbstractLocalVariable variable) {
-        if (variablesOnLeft == null) {
-            variablesOnLeft = new HashSet<>();
-            variablesOnLeft.add(variable);
-            variable.addVariableOnRight(this);
-        } else if (!variablesOnLeft.contains(variable)) {
-            variablesOnLeft.add(variable);
-            variable.addVariableOnRight(this);
-        }
-    }
+	public DefaultList<LocalVariableReference> getReferences() {
+		return references;
+	}
 
-    protected void addVariableOnRight(AbstractLocalVariable variable) {
-        if (variablesOnRight == null) {
-            variablesOnRight = new HashSet<>();
-            variablesOnRight.add(variable);
-            variable.addVariableOnLeft(this);
-        } else if (!variablesOnRight.contains(variable)) {
-            variablesOnRight.add(variable);
-            variable.addVariableOnLeft(this);
-        }
-    }
+	public void addReference(LocalVariableReference reference) {
+		references.add(reference);
+	}
 
-    public boolean isPrimitiveLocalVariable() { return false; }
+	/**
+	 * Determines if the local variable represented by this object is either the
+	 * same as, or is a super type variable of, the local variable represented by
+	 * the specified parameter.
+	 */
+	public abstract boolean isAssignableFrom(Map<String, BaseType> typeBounds, Type type);
+
+	public abstract void typeOnRight(Map<String, BaseType> typeBounds, Type type);
+
+	public abstract void typeOnLeft(Map<String, BaseType> typeBounds, Type type);
+
+	public abstract boolean isAssignableFrom(Map<String, BaseType> typeBounds, AbstractLocalVariable variable);
+
+	public abstract void variableOnRight(Map<String, BaseType> typeBounds, AbstractLocalVariable variable);
+
+	public abstract void variableOnLeft(Map<String, BaseType> typeBounds, AbstractLocalVariable variable);
+
+	protected void fireChangeEvent(Map<String, BaseType> typeBounds) {
+		if (variablesOnLeft != null) {
+			for (AbstractLocalVariable v : variablesOnLeft) {
+				v.variableOnRight(typeBounds, this);
+			}
+		}
+		if (variablesOnRight != null) {
+			for (AbstractLocalVariable v : variablesOnRight) {
+				v.variableOnLeft(typeBounds, this);
+			}
+		}
+	}
+
+	protected void addVariableOnLeft(AbstractLocalVariable variable) {
+		if (variablesOnLeft == null) {
+			variablesOnLeft = new HashSet<>();
+			variablesOnLeft.add(variable);
+			variable.addVariableOnRight(this);
+		} else if (!variablesOnLeft.contains(variable)) {
+			variablesOnLeft.add(variable);
+			variable.addVariableOnRight(this);
+		}
+	}
+
+	protected void addVariableOnRight(AbstractLocalVariable variable) {
+		if (variablesOnRight == null) {
+			variablesOnRight = new HashSet<>();
+			variablesOnRight.add(variable);
+			variable.addVariableOnLeft(this);
+		} else if (!variablesOnRight.contains(variable)) {
+			variablesOnRight.add(variable);
+			variable.addVariableOnLeft(this);
+		}
+	}
+
+	public boolean isPrimitiveLocalVariable() {
+		return false;
+	}
 }
--- src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/model/localvariable/ObjectLocalVariable.java
+++ src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/model/localvariable/ObjectLocalVariable.java
@@ -7,183 +7,187 @@
 
 package org.jd.core.v1.service.converter.classfiletojavasyntax.model.localvariable;
 
+import static org.jd.core.v1.model.javasyntax.type.ObjectType.TYPE_OBJECT;
+import static org.jd.core.v1.model.javasyntax.type.ObjectType.TYPE_UNDEFINED_OBJECT;
+
+import java.util.Map;
+
 import org.jd.core.v1.model.javasyntax.type.BaseType;
 import org.jd.core.v1.model.javasyntax.type.ObjectType;
 import org.jd.core.v1.model.javasyntax.type.Type;
 import org.jd.core.v1.service.converter.classfiletojavasyntax.util.TypeMaker;
 
-import java.util.Map;
-
-import static org.jd.core.v1.model.javasyntax.type.ObjectType.TYPE_OBJECT;
-import static org.jd.core.v1.model.javasyntax.type.ObjectType.TYPE_UNDEFINED_OBJECT;
-
 public class ObjectLocalVariable extends AbstractLocalVariable {
-    protected TypeMaker typeMaker;
-    protected Type type;
+	protected TypeMaker typeMaker;
+	protected Type type;
 
-    public ObjectLocalVariable(TypeMaker typeMaker, int index, int offset, Type type, String name) {
-        super(index, offset, name);
-        this.typeMaker = typeMaker;
-        this.type = type;
-    }
+	public ObjectLocalVariable(TypeMaker typeMaker, int index, int offset, Type type, String name) {
+		super(index, offset, name);
+		this.typeMaker = typeMaker;
+		this.type = type;
+	}
 
-    public ObjectLocalVariable(TypeMaker typeMaker, int index, int offset, Type type, String name, boolean declared) {
-        this(typeMaker, index, offset, type, name);
-        this.declared = declared;
-    }
+	public ObjectLocalVariable(TypeMaker typeMaker, int index, int offset, Type type, String name, boolean declared) {
+		this(typeMaker, index, offset, type, name);
+		this.declared = declared;
+	}
 
-    public ObjectLocalVariable(TypeMaker typeMaker, int index, int offset, ObjectLocalVariable objectLocalVariable) {
-        super(index, offset, null);
-        this.typeMaker = typeMaker;
-        this.type = objectLocalVariable.type;
-    }
+	public ObjectLocalVariable(TypeMaker typeMaker, int index, int offset, ObjectLocalVariable objectLocalVariable) {
+		super(index, offset, null);
+		this.typeMaker = typeMaker;
+		this.type = objectLocalVariable.type;
+	}
 
-    @Override
-    public Type getType() {
-        return type;
-    }
+	@Override
+	public Type getType() {
+		return type;
+	}
 
-    public void setType(Map<String, BaseType> typeBounds, Type type) {
-        if (!this.type.equals(type)) {
-            this.type = type;
-            fireChangeEvent(typeBounds);
-        }
-    }
+	public void setType(Map<String, BaseType> typeBounds, Type type) {
+		if (!this.type.equals(type)) {
+			this.type = type;
+			fireChangeEvent(typeBounds);
+		}
+	}
 
-    @Override
-    public int getDimension() {
-        return type.getDimension();
-    }
+	@Override
+	public int getDimension() {
+		return type.getDimension();
+	}
 
-    @Override
-    public void accept(LocalVariableVisitor visitor) {
-        visitor.visit(this);
-    }
+	@Override
+	public void accept(LocalVariableVisitor visitor) {
+		visitor.visit(this);
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
 
-        sb.append("ObjectLocalVariable{");
-        
-        if (type.getName() == null) {
-            sb.append(type.getInternalName());
-        } else {
-            sb.append(type.getName());
-        }
+		sb.append("ObjectLocalVariable{");
 
-        if (type.getDimension() > 0) {
-            sb.append(new String(new char[type.getDimension()]).replaceAll("\0", "[]"));
-        }
+		if (type.getName() == null) {
+			sb.append(type.getInternalName());
+		} else {
+			sb.append(type.getName());
+		}
 
-        sb.append(' ').append(name).append(", index=").append(index);
+		if (type.getDimension() > 0) {
+			sb.append(new String(new char[type.getDimension()]).replaceAll("\0", "[]"));
+		}
 
-        if (next != null) {
-            sb.append(", next=").append(next);
-        }
+		sb.append(' ').append(name).append(", index=").append(index);
 
-        return sb.append("}").toString();
-    }
+		if (next != null) {
+			sb.append(", next=").append(next);
+		}
 
-    @Override
-    public boolean isAssignableFrom(Map<String, BaseType> typeBounds, Type type) {
-        if (this.type.isObjectType()) {
-            if (this.type.equals(TYPE_OBJECT)) {
-                if ((type.getDimension() > 0) || !type.isPrimitiveType()) {
-                    return true;
-                }
-            }
+		return sb.append("}").toString();
+	}
 
-            if (type.isObjectType()) {
-                return typeMaker.isAssignable(typeBounds, (ObjectType) this.type, (ObjectType) type);
-            }
-        }
+	@Override
+	public boolean isAssignableFrom(Map<String, BaseType> typeBounds, Type type) {
+		if (this.type.isObjectType()) {
+			if (this.type.equals(TYPE_OBJECT)) {
+				if ((type.getDimension() > 0) || !type.isPrimitiveType()) {
+					return true;
+				}
+			}
 
-        return false;
-    }
+			if (type.isObjectType()) {
+				return typeMaker.isAssignable(typeBounds, (ObjectType) this.type, (ObjectType) type);
+			}
+		}
 
-    @Override
-    public void typeOnRight(Map<String, BaseType> typeBounds, Type type) {
-        if (type != TYPE_UNDEFINED_OBJECT) {
-            if (this.type == TYPE_UNDEFINED_OBJECT) {
-                this.type = type;
-                fireChangeEvent(typeBounds);
-            } else if ((this.type.getDimension() == 0) && (type.getDimension() == 0)) {
-                assert !this.type.isPrimitiveType() && !type.isPrimitiveType() : "ObjectLocalVariable.typeOnRight(type) : unexpected type";
+		return false;
+	}
 
-                if (this.type.isObjectType()) {
-                    ObjectType thisObjectType = (ObjectType) this.type;
+	@Override
+	public void typeOnRight(Map<String, BaseType> typeBounds, Type type) {
+		if (type != TYPE_UNDEFINED_OBJECT) {
+			if (this.type == TYPE_UNDEFINED_OBJECT) {
+				this.type = type;
+				fireChangeEvent(typeBounds);
+			} else if ((this.type.getDimension() == 0) && (type.getDimension() == 0)) {
+				assert !this.type.isPrimitiveType() && !type.isPrimitiveType()
+						: "ObjectLocalVariable.typeOnRight(type) : unexpected type";
 
-                    if (type.isObjectType()) {
-                        ObjectType otherObjectType = (ObjectType) type;
+				if (this.type.isObjectType()) {
+					ObjectType thisObjectType = (ObjectType) this.type;
 
-                        if (thisObjectType.getInternalName().equals(otherObjectType.getInternalName())) {
-                            if ((thisObjectType.getTypeArguments() == null) && (otherObjectType.getTypeArguments() != null)) {
-                                // Keep type, update type arguments
-                                this.type = otherObjectType;
-                                fireChangeEvent(typeBounds);
-                            }
-                        } else if (typeMaker.isAssignable(typeBounds, thisObjectType, otherObjectType)) {
-                            // Assignable types
-                            if ((thisObjectType.getTypeArguments() == null) && (otherObjectType.getTypeArguments() != null)) {
-                                // Keep type, update type arguments
-                                this.type = thisObjectType.createType(otherObjectType.getTypeArguments());
-                                fireChangeEvent(typeBounds);
-                            }
-                        }
-                    }
-                } else if (this.type.isGenericType()) {
-                    if (type.isGenericType()) {
-                        this.type = type;
-                        fireChangeEvent(typeBounds);
-                    }
-                }
-            }
-        }
-    }
+					if (type.isObjectType()) {
+						ObjectType otherObjectType = (ObjectType) type;
 
-    @Override
-    public void typeOnLeft(Map<String, BaseType> typeBounds, Type type) {
-        if ((type != TYPE_UNDEFINED_OBJECT) && !type.equals(TYPE_OBJECT)) {
-            if (this.type == TYPE_UNDEFINED_OBJECT) {
-                this.type = type;
-                fireChangeEvent(typeBounds);
-            } else if ((this.type.getDimension() == 0) && (type.getDimension() == 0) && this.type.isObjectType() && type.isObjectType()) {
-                ObjectType thisObjectType = (ObjectType) this.type;
-                ObjectType otherObjectType = (ObjectType) type;
+						if (thisObjectType.getInternalName().equals(otherObjectType.getInternalName())) {
+							if ((thisObjectType.getTypeArguments() == null)
+									&& (otherObjectType.getTypeArguments() != null)) {
+								// Keep type, update type arguments
+								this.type = otherObjectType;
+								fireChangeEvent(typeBounds);
+							}
+						} else if (typeMaker.isAssignable(typeBounds, thisObjectType, otherObjectType)) {
+							// Assignable types
+							if ((thisObjectType.getTypeArguments() == null)
+									&& (otherObjectType.getTypeArguments() != null)) {
+								// Keep type, update type arguments
+								this.type = thisObjectType.createType(otherObjectType.getTypeArguments());
+								fireChangeEvent(typeBounds);
+							}
+						}
+					}
+				} else if (this.type.isGenericType()) {
+					if (type.isGenericType()) {
+						this.type = type;
+						fireChangeEvent(typeBounds);
+					}
+				}
+			}
+		}
+	}
 
-                if (thisObjectType.getInternalName().equals(otherObjectType.getInternalName())) {
-                    if ((thisObjectType.getTypeArguments() == null) && (otherObjectType.getTypeArguments() != null)) {
-                        // Keep type, update type arguments
-                        this.type = otherObjectType;
-                        fireChangeEvent(typeBounds);
-                    }
-                } else if (typeMaker.isAssignable(typeBounds, otherObjectType, thisObjectType)) {
-                    // Assignable types
-                    if ((thisObjectType.getTypeArguments() == null) && (otherObjectType.getTypeArguments() != null)) {
-                        // Keep type, update type arguments
-                        this.type = thisObjectType.createType(otherObjectType.getTypeArguments());
-                        fireChangeEvent(typeBounds);
-                    }
-                }
-            }
-        }
-    }
+	@Override
+	public void typeOnLeft(Map<String, BaseType> typeBounds, Type type) {
+		if ((type != TYPE_UNDEFINED_OBJECT) && !type.equals(TYPE_OBJECT)) {
+			if (this.type == TYPE_UNDEFINED_OBJECT) {
+				this.type = type;
+				fireChangeEvent(typeBounds);
+			} else if ((this.type.getDimension() == 0) && (type.getDimension() == 0) && this.type.isObjectType()
+					&& type.isObjectType()) {
+				ObjectType thisObjectType = (ObjectType) this.type;
+				ObjectType otherObjectType = (ObjectType) type;
 
-    @Override
-    public boolean isAssignableFrom(Map<String, BaseType> typeBounds, AbstractLocalVariable variable) {
-        return isAssignableFrom(typeBounds, variable.getType());
-    }
+				if (thisObjectType.getInternalName().equals(otherObjectType.getInternalName())) {
+					if ((thisObjectType.getTypeArguments() == null) && (otherObjectType.getTypeArguments() != null)) {
+						// Keep type, update type arguments
+						this.type = otherObjectType;
+						fireChangeEvent(typeBounds);
+					}
+				} else if (typeMaker.isAssignable(typeBounds, otherObjectType, thisObjectType)) {
+					// Assignable types
+					if ((thisObjectType.getTypeArguments() == null) && (otherObjectType.getTypeArguments() != null)) {
+						// Keep type, update type arguments
+						this.type = thisObjectType.createType(otherObjectType.getTypeArguments());
+						fireChangeEvent(typeBounds);
+					}
+				}
+			}
+		}
+	}
 
-    @Override
-    public void variableOnRight(Map<String, BaseType> typeBounds, AbstractLocalVariable variable) {
-        addVariableOnRight(variable);
-        typeOnRight(typeBounds, variable.getType());
-    }
+	@Override
+	public boolean isAssignableFrom(Map<String, BaseType> typeBounds, AbstractLocalVariable variable) {
+		return isAssignableFrom(typeBounds, variable.getType());
+	}
 
-    @Override
-    public void variableOnLeft(Map<String, BaseType> typeBounds, AbstractLocalVariable variable) {
-        addVariableOnLeft(variable);
-        typeOnLeft(typeBounds, variable.getType());
-    }
+	@Override
+	public void variableOnRight(Map<String, BaseType> typeBounds, AbstractLocalVariable variable) {
+		addVariableOnRight(variable);
+		typeOnRight(typeBounds, variable.getType());
+	}
+
+	@Override
+	public void variableOnLeft(Map<String, BaseType> typeBounds, AbstractLocalVariable variable) {
+		addVariableOnLeft(variable);
+		typeOnLeft(typeBounds, variable.getType());
+	}
 }
