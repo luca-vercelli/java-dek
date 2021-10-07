b49821b800cad6af469d46899a326581d8f9841d
Luca Vercelli <luca.vercelli@it-present.com>
2021-10-07 14:06:14 +0200

    clean

--- src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/util/AbstractTypeParametersToTypeArgumentsBinder.java
+++ src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/util/AbstractTypeParametersToTypeArgumentsBinder.java
@@ -7,41 +7,55 @@
 
 package org.jd.core.v1.service.converter.classfiletojavasyntax.util;
 
-import org.jd.core.v1.model.javasyntax.expression.*;
-import org.jd.core.v1.model.javasyntax.type.*;
-import org.jd.core.v1.service.converter.classfiletojavasyntax.model.javasyntax.expression.*;
+import org.jd.core.v1.model.javasyntax.expression.BaseExpression;
+import org.jd.core.v1.model.javasyntax.expression.Expression;
+import org.jd.core.v1.model.javasyntax.expression.FieldReferenceExpression;
+import org.jd.core.v1.model.javasyntax.type.BaseType;
+import org.jd.core.v1.model.javasyntax.type.ObjectType;
+import org.jd.core.v1.model.javasyntax.type.Type;
+import org.jd.core.v1.model.javasyntax.type.Types;
+import org.jd.core.v1.service.converter.classfiletojavasyntax.model.javasyntax.expression.ClassFileConstructorInvocationExpression;
+import org.jd.core.v1.service.converter.classfiletojavasyntax.model.javasyntax.expression.ClassFileMethodInvocationExpression;
+import org.jd.core.v1.service.converter.classfiletojavasyntax.model.javasyntax.expression.ClassFileNewExpression;
+import org.jd.core.v1.service.converter.classfiletojavasyntax.model.javasyntax.expression.ClassFileSuperConstructorInvocationExpression;
 
 public abstract class AbstractTypeParametersToTypeArgumentsBinder {
-    public abstract ClassFileConstructorInvocationExpression newConstructorInvocationExpression(
-            int lineNumber, ObjectType objectType, String descriptor,
-            TypeMaker.MethodTypes methodTypes, BaseExpression parameters);
+	public abstract ClassFileConstructorInvocationExpression newConstructorInvocationExpression(int lineNumber,
+			ObjectType objectType, String descriptor, TypeMaker.MethodTypes methodTypes, BaseExpression parameters);
 
-    public abstract ClassFileSuperConstructorInvocationExpression newSuperConstructorInvocationExpression(
-            int lineNumber, ObjectType objectType, String descriptor,
-            TypeMaker.MethodTypes methodTypes, BaseExpression parameters);
+	public abstract ClassFileSuperConstructorInvocationExpression newSuperConstructorInvocationExpression(
+			int lineNumber, ObjectType objectType, String descriptor, TypeMaker.MethodTypes methodTypes,
+			BaseExpression parameters);
 
-    public abstract ClassFileMethodInvocationExpression newMethodInvocationExpression(
-            int lineNumber, Expression expression, ObjectType objectType, String name, String descriptor,
-            TypeMaker.MethodTypes methodTypes, BaseExpression parameters);
+	public abstract ClassFileMethodInvocationExpression newMethodInvocationExpression(int lineNumber,
+			Expression expression, ObjectType objectType, String name, String descriptor,
+			TypeMaker.MethodTypes methodTypes, BaseExpression parameters);
 
-    public abstract FieldReferenceExpression newFieldReferenceExpression(
-            int lineNumber, Type type, Expression expression, ObjectType objectType, String name, String descriptor);
+	public abstract FieldReferenceExpression newFieldReferenceExpression(int lineNumber, Type type,
+			Expression expression, ObjectType objectType, String name, String descriptor);
 
-    public abstract void bindParameterTypesWithArgumentTypes(Type type, Expression expression);
+	public abstract void bindParameterTypesWithArgumentTypes(Type type, Expression expression);
 
-    public void updateNewExpression(ClassFileNewExpression ne, String descriptor, TypeMaker.MethodTypes methodTypes, BaseExpression parameters) {
-        ne.set(descriptor, clone(methodTypes.parameterTypes), parameters);
-    }
+	public void updateNewExpression(ClassFileNewExpression ne, String descriptor, TypeMaker.MethodTypes methodTypes,
+			BaseExpression parameters) {
+		ne.set(descriptor, clone(methodTypes.parameterTypes), parameters);
+	}
 
-    protected static BaseType clone(BaseType parameterTypes) {
-        if ((parameterTypes != null) && parameterTypes.isList()) {
-            switch (parameterTypes.size()) {
-                case 0: parameterTypes = null; break;
-                case 1: parameterTypes = parameterTypes.getFirst(); break;
-                default: parameterTypes = new Types(parameterTypes.getList()); break;
-            }
-        }
+	protected static BaseType clone(BaseType parameterTypes) {
+		if ((parameterTypes != null) && parameterTypes.isList()) {
+			switch (parameterTypes.size()) {
+			case 0:
+				parameterTypes = null;
+				break;
+			case 1:
+				parameterTypes = parameterTypes.getFirst();
+				break;
+			default:
+				parameterTypes = new Types(parameterTypes.getList());
+				break;
+			}
+		}
 
-        return parameterTypes;
-    }
+		return parameterTypes;
+	}
 }
