413f732e8eafa6e015697570b81f650d4814b0ff
Luca Vercelli <luca.vercelli@it-present.com>
2021-10-12 11:55:07 +0200

    test

---
+++ src/test/java/org/jd/core/v1/cli/TestDecompile.java
@@ -0,0 +1,26 @@
+package org.jd.core.v1.cli;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TestDecompile {
+
+	@Test
+	@Ignore
+	// This test halts because of System.exit()
+	public void testDecompileZip() throws URISyntaxException {
+		File fileSrc = new File(TestDecompile.class.getResource("/zip/data-java-jdk-1.8.0.zip").toURI());
+		File fileDstFolder = new File("target" + File.separator + "tmp");
+		CliMain.main(new String[] { "-d", fileDstFolder.getPath(), fileSrc.getPath() });
+
+		String destFilePath = "target/tmp/org/jd/core/test/Basic.class".replace("/", File.separator);
+		File fileDst = new File(destFilePath);
+		assertTrue("No decompiled file: " + fileDst.getAbsolutePath(), fileDst.exists());
+		assertTrue("Decompiled file empty: " + fileDst.getAbsolutePath(), fileDst.length() > 0);
+	}
+}
