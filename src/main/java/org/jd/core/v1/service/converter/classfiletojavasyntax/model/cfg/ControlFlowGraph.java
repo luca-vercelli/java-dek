4e1c6374b1f2d8934cd0874e627b8bb6a6d4b93b
Luca Vercelli <luca.vercelli@it-present.com>
2021-10-13 12:45:45 +0200

    toString

--- src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/model/cfg/ControlFlowGraph.java
+++ src/main/java/org/jd/core/v1/service/converter/classfiletojavasyntax/model/cfg/ControlFlowGraph.java
@@ -19,92 +19,97 @@ import org.jd.core.v1.util.DefaultList;
  * The object contains a structure of BasicBlock's.
  */
 public class ControlFlowGraph {
-    protected Method method;
-    protected List<BasicBlock> list = new DefaultList<BasicBlock>() {
+	protected Method method;
+	protected List<BasicBlock> list = new DefaultList<BasicBlock>() {
 
 		private static final long serialVersionUID = -6085305152533533391L;
 
 		public BasicBlock remove(int index) {
-            throw new RuntimeException("Unexpected call");
-        }
-    };
-    protected int[] offsetToLineNumbers = null;
+			throw new RuntimeException("Unexpected call");
+		}
+	};
+	protected int[] offsetToLineNumbers = null;
 
-    public ControlFlowGraph(Method method) {
-        this.method = method;
-    }
+	public ControlFlowGraph(Method method) {
+		this.method = method;
+	}
 
-    public Method getMethod() {
-        return method;
-    }
+	public Method getMethod() {
+		return method;
+	}
 
-    public List<BasicBlock> getBasicBlocks() {
-        return list;
-    }
+	public List<BasicBlock> getBasicBlocks() {
+		return list;
+	}
 
-    /**
-     * Return first block in internal list, it should be a START block
-     */
-    public BasicBlock getStart() {
-        return list.get(0);
-    }
+	/**
+	 * Return first block in internal list, it should be a START block
+	 */
+	public BasicBlock getStart() {
+		return list.get(0);
+	}
 
-    /**
-     * Add a new BasicBlock in the graph, with same features as <code>original</code>
-     * 
-     * @param original
-     * @return new block
-     */
-    public BasicBlock newBasicBlock(BasicBlock original) {
-        BasicBlock basicBlock = new BasicBlock(this, list.size(), original);
-        list.add(basicBlock);
-        return basicBlock;
-    }
+	/**
+	 * Add a new BasicBlock in the graph, with same features as
+	 * <code>original</code>
+	 * 
+	 * @param original
+	 * @return new block
+	 */
+	public BasicBlock newBasicBlock(BasicBlock original) {
+		BasicBlock basicBlock = new BasicBlock(this, list.size(), original);
+		list.add(basicBlock);
+		return basicBlock;
+	}
 
-    /**
-     * Add a new BasicBlock in the graph
-     */
-    public BasicBlock newBasicBlock(int fromOffset, int toOffset) {
-        return newBasicBlock(0, fromOffset, toOffset);
-    }
+	/**
+	 * Add a new BasicBlock in the graph
+	 */
+	public BasicBlock newBasicBlock(int fromOffset, int toOffset) {
+		return newBasicBlock(0, fromOffset, toOffset);
+	}
 
-    /**
-     * Add a new BasicBlock in the graph
-     */
-    public BasicBlock newBasicBlock(int type, int fromOffset, int toOffset) {
-        BasicBlock basicBlock = new BasicBlock(this, list.size(), type, fromOffset, toOffset, true);
-        list.add(basicBlock);
-        return basicBlock;
-    }
+	/**
+	 * Add a new BasicBlock in the graph
+	 */
+	public BasicBlock newBasicBlock(int type, int fromOffset, int toOffset) {
+		BasicBlock basicBlock = new BasicBlock(this, list.size(), type, fromOffset, toOffset, true);
+		list.add(basicBlock);
+		return basicBlock;
+	}
 
-    /**
-     * Add a new BasicBlock in the graph
-     */
-    public BasicBlock newBasicBlock(int type, int fromOffset, int toOffset, boolean inverseCondition) {
-        BasicBlock basicBlock = new BasicBlock(this, list.size(), type, fromOffset, toOffset, inverseCondition);
-        list.add(basicBlock);
-        return basicBlock;
-    }
+	/**
+	 * Add a new BasicBlock in the graph
+	 */
+	public BasicBlock newBasicBlock(int type, int fromOffset, int toOffset, boolean inverseCondition) {
+		BasicBlock basicBlock = new BasicBlock(this, list.size(), type, fromOffset, toOffset, inverseCondition);
+		list.add(basicBlock);
+		return basicBlock;
+	}
 
-    /**
-     * Add a new BasicBlock in the graph
-     */
-    public BasicBlock newBasicBlock(int type, int fromOffset, int toOffset, Set<BasicBlock> predecessors) {
-        BasicBlock basicBlock = new BasicBlock(this, list.size(), type, fromOffset, toOffset, true, predecessors);
-        list.add(basicBlock);
-        return basicBlock;
-    }
+	/**
+	 * Add a new BasicBlock in the graph
+	 */
+	public BasicBlock newBasicBlock(int type, int fromOffset, int toOffset, Set<BasicBlock> predecessors) {
+		BasicBlock basicBlock = new BasicBlock(this, list.size(), type, fromOffset, toOffset, true, predecessors);
+		list.add(basicBlock);
+		return basicBlock;
+	}
 
-    public void setOffsetToLineNumbers(int[] offsetToLineNumbers) {
-        this.offsetToLineNumbers = offsetToLineNumbers;
-    }
+	public void setOffsetToLineNumbers(int[] offsetToLineNumbers) {
+		this.offsetToLineNumbers = offsetToLineNumbers;
+	}
 
-    public int getLineNumber(int offset) {
-        return (offsetToLineNumbers == null) ? 0 : offsetToLineNumbers[offset];
-    }
-   
+	public int getLineNumber(int offset) {
+		return (offsetToLineNumbers == null) ? 0 : offsetToLineNumbers[offset];
+	}
+
 	@Override
 	public String toString() {
-		return "ControlFlowGraph for " + method;
+		String s = "ControlFlowGraph for " + method + ":\r\n";
+		for (BasicBlock l : list) {
+			s += l.getTypeName() + "(" + l.getIndex() + ") " + l.getFromOffset() + "-" + l.getToOffset() + "\r\n";
+		}
+		return s;
 	}
 }
